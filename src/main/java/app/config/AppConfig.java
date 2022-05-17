3222d5f93e1380144e2b3c587776354e7cfddfcc
Ivan <andvan2016@mail.ru>
2022-05-17 20:21:50 +0700

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,13 @@
+package app.config;
+
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+}
